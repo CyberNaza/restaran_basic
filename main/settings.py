ab94d094127475e0bafa8c498873e3ccb5838c5e
nazarbek <nazarbeknazarov772@gmail.com>
2025-08-16 01:02:17 +0500

    1

--- main/settings.py
+++ main/settings.py
@@ -165,7 +165,7 @@ SWAGGER_SETTINGS = {
 
 TELEGRAM_BOT_TOKEN = "8478003974:AAEf1RHpMJ8Aw0pAYx_Q2XtzbctAgY7V3EM"
 BOT_API_TOKEN = "123"  # must match bot server
-DEBUG = False
+DEBUG = True
 
 # Allow all hosts
 ALLOWED_HOSTS = ["*"]
